e5db7ba35cbfd0236bf5a8c1ba880903445acdbf
Kondratenko Konstia <evpatyikalovrat@gmail.com>
2022-12-14 22:20:32 -0500

    14/12/2022

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 
-const quarterOf = (month) => {
-   // Your code here
-   if (month <=3){
-     return 1
-   }else if(month <=6 && month >=4){
-     return 2
-   }else if(month <=9 && month >=7){
-     return 3
- }else if(month <=12 && month >=10){
-     return 4
-   };
-   }
-   console.log(quarterOf(3))
-   console.log(quarterOf(8))
-   console.log(quarterOf(11))
+// const quarterOf = (month) => {
+//    // Your code here
+//    if (month <=3){
+//      return 1
+//    }else if(month <=6 && month >=4){
+//      return 2
+//    }else if(month <=9 && month >=7){
+//      return 3
+//  }else if(month <=12 && month >=10){
+//      return 4
+//    };
+//    }
+//    console.log(quarterOf(3))
+//    console.log(quarterOf(8))
+//    console.log(quarterOf(11))
+
+
+
+//14.12.20222 *********************************************************************************************************************************************
+
+
+
+// function findMultiples(integer, limit) {
+//   let arr = [];
+//  const int = integer;
+//  while(integer <= limit){
+//    arr.push(integer);
+//    integer+=int
+//  }
+ 
+ 
+//  console.log(arr)
+// }
+// findMultiples(5,25);
